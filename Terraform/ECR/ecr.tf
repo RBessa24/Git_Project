7f0a2630f384d51a839bb80d0fb125a0222ad17f
RBessa24 <ricardo.bessa@ritain.io>
2023-05-04 10:24:48 +0100

    Scan Terraform Scripts 1

--- Terraform/ECR/ecr.tf
+++ Terraform/ECR/ecr.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
 #####################################
 resource "aws_ecr_repository" "rio-academy" {
   name = "my-first-ecr-repo" # Naming my repository
