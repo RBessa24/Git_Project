e9357c2de311addae6f88b1ac0529f03995750b2
RBessa24 <ricardo.bessa@ritain.io>
2023-05-09 15:06:11 +0100

    Final tests

--- Terraform/ECS/ecs.tf
+++ Terraform/ECS/ecs.tf
@@ -121,10 +121,11 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-r#esource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole2_policy" {
+/*resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole2_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole2.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+  } */
+
 
 resource "aws_ecs_cluster" "my_cluster" {
   name = "my-cluster" # Naming the cluster
