71ecaae791795038fef51522663c20a04e3c6e4b
RBessa24 <ricardo.bessa@ritain.io>
2023-05-09 20:28:36 +0100

    Fix Errors

--- Terraform/ECS/ecs.tf
+++ Terraform/ECS/ecs.tf
@@ -120,10 +120,10 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-/*resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole2_policy" {
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole2_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole2.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  } */
+  }
 
 
 resource "aws_ecs_cluster" "my_cluster" {
