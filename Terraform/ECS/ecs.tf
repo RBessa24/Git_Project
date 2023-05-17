b1637023395ed8310d93764d64b8edb597467488
RBessa24 <ricardo.bessa@ritain.io>
2023-05-17 02:38:35 +0100

    Testing

--- Terraform/ECS/ecs.tf
+++ Terraform/ECS/ecs.tf
@@ -318,7 +318,7 @@ resource "aws_lb_listener" "listener" {
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target grou
   }
 }
 
